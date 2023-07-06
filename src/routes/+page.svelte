25d4fda0dc4ef6716a00140aa530bcb54dbc69a5
strelda <strelda@protonmail.com>
2023-07-06 11:02:04 +0200

    working backend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let selection = "";
+    
+    function vote() {
+      fetch('http://localhost:5000/vote', {
+        method: 'POST',
+        body: JSON.stringify({ option: selection }),
+        headers: { 'Content-Type': 'application/json' },
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => console.log(data))
+      .catch(error => {
+        console.log('There was a problem with the fetch operation: ', error.message);
+      });
+    }
+  
+    const options = ['A', 'B', 'C', 'D'];
+</script>
+  
+
+<div>
+  {#each options as option (option)}
+    <label>
+      <input type="radio" bind:group={selection} value={option}>
+      {option}
+    </label>
+  {/each}
+</div>
+
+<button on:click={vote}>Vote</button> 
+
---
+++ src/routes/results/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { onMount } from 'svelte';
+    
+    let results = {};
+  
+    onMount(async () => {
+      const res = await fetch('http://localhost:5000/results');
+      results = await res.json();
+    });
+  </script>
+  
+<h1>Vote results</h1>
+<ul>
+{#each Object.entries(results) as [option, count] (option)}
+    <li>{option}: {count}</li>
+{/each}
+</ul>
