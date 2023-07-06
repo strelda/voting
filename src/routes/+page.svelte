245fb8fc1765f2fd106f363cc6dfb3cbebaa4170
strelda <strelda@protonmail.com>
2023-07-06 11:45:39 +0200

    voting is nice now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
-<script>
+<script lang='ts'>
     let selection = "";
-    
+    function selectOption(option: string): void {
+    selection = option;
+  }
+
     function vote() {
       fetch('http://localhost:5000/vote', {
         method: 'POST',
@@ -21,16 +24,56 @@
   
     const options = ['A', 'B', 'C', 'D'];
 </script>
-  
 
-<div>
-  {#each options as option (option)}
-    <label>
-      <input type="radio" bind:group={selection} value={option}>
-      {option}
-    </label>
-  {/each}
+<div class='grid'>
+  <div class="button-grid">
+    {#each options as option (option)}
+      <button on:click={() => selectOption(option)} class:selected={selection === option}>{option}</button>
+    {/each}
+  </div>
+
+  <button class="vote-button" on:click={vote}>Vote</button>
 </div>
 
-<button on:click={vote}>Vote</button> 
+<style>
+  .grid {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    gap: 10px;
+  }
 
+  .button-grid {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    gap: 10px;
+    width: 90%;
+  }
+
+  .button-grid button {
+    flex: 1;
+    padding: 30px;
+    background-color: rgb(41, 71, 71);
+    color: white;
+    border: none;
+    text-align: center;
+    cursor: pointer;
+    border-radius: 20px;
+  }
+
+  .button-grid button.selected {
+    background-color: rgb(0, 158, 150);
+  }
+
+  .vote-button {
+    padding: 10px 20px;
+    background-color: rgb(35, 146, 70);
+    color: white;
+    border: none;
+    cursor: pointer;
+    border-radius: 20px;
+  }
+</style>
